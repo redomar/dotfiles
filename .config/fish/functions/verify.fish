f51f68b76fe1dfd0464d5bce7b2e21ac8dc27778
Mohamed <mohamed12@live.co.uk>
2023-12-31 22:15:14 +0000

    2023-12-31

--- .config/fish/functions/verify.fish
+++ .config/fish/functions/verify.fish
@@ -34,11 +34,13 @@ function verify --description 'Compare CRC with Filename CRC' -a all
   end
 
   set -lx hash (crc32 $argv 2>/dev/null | tr a-z A-Z | cut -c 1-8 | grep -Ee '[0-9A-F]{8}' -o)
+  set -l filename (basename $argv)
 
   if test "$hash" = "$fileHash"
     echo -e "Checksum Passed \e[30;42m $hash = $fileHash \e[0m"
   else
-    echo -e "\e[30;41mChecksum Failed $hash != $fileHash \e[5;31;40m UNSAFE -> \e[0;4;93m$argv\e[0m"
+    echo -e "\e[30;41mChecksum Failed $hash != $fileHash \e[5;31;40m UNSAFE -> \e[0;4;93m$filename\e[0m"
     return 2
   end
 end
+
